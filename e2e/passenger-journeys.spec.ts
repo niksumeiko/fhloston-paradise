0d932d0879047d517492a64ae72ed693af8a5799
Jose Garrera <josegarrera@gmail.com>
2026-03-24 15:51:50 -0300

    test: allows a passenger to leave the ship (e2e)

--- e2e/passenger-journeys.spec.ts
+++ e2e/passenger-journeys.spec.ts
@@ -20,4 +20,17 @@ test.describe('The passenger journeys', () => {
 
         await expect(page.getByText('Invalid email or password')).toBeVisible();
     });
+
+    test('allows a passenger to leave the ship', async ({ page }) => {
+        await page.goto('/login');
+
+        await page.getByLabel('Email').fill('korben@fhloston.com');
+        await page.getByLabel('Password').fill('multipass');
+        await page.getByRole('button', { name: 'Login' }).click();
+        await expect(page.getByText('Welcome, Korben Dallas')).toBeVisible();
+
+        await page.getByRole('button', { name: 'Logout' }).click();
+
+        await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+    });
 });
