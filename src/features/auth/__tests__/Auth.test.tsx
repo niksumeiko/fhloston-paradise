6d42291f9b8f015cddeedd188b40d5f0ed6d3095
Nik Sumeiko <nik.sumeiko@gmail.com>
2026-04-01 10:28:04 +0200

    test: Errors presence for invalid credentials

--- src/features/auth/__tests__/Auth.test.tsx
+++ src/features/auth/__tests__/Auth.test.tsx
@@ -27,4 +27,26 @@ describe('Authentication', () => {
         cy.url().should('equal', '/');
         cy.contains('Korben Dallas').should('be.visible');
     });
+
+    it('see errors for invalid credentials', () => {
+        cy.intercept('POST', '**/api/login**', {
+            statusCode: 401,
+            body: { message: 'Invalid credentials' },
+        });
+
+        cy.mount(<App />, '/login');
+
+        cy.findByLabelText('Email').type('korben');
+        cy.findByLabelText('Password').type('xyz');
+        cy.findByRole('button', { name: 'Login' }).click();
+
+        cy.contains('Please enter a valid email').should('be.visible');
+        cy.contains('Password must be at least 6 characters').should('be.visible');
+
+        cy.findByLabelText('Email').clear().type('korben@fhloston.com');
+        cy.findByLabelText('Password').clear().type('xyzxyz');
+        cy.findByRole('button', { name: 'Login' }).click();
+
+        cy.contains('Invalid credentials').should('be.visible');
+    });
 });
